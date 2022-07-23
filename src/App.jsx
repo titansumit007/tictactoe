474a68edcf30eb09e9c4c401dee4d3c4dbab9bdb
Sumit Chowdhary <titansumit007@gmail.com>
2022-07-24 03:03:06 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App= () => {
+  return (
+    <>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+    <p>First build</p>
+  </>
+
+
+  )
+}
+export default App;
